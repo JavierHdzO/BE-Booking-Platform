4a28a3271bc6fddd3fe7e5a9763c4c7780589c04
JavierHdzO <fco_hernandez1705@hotmail.com>
2022-10-05 21:05:08 -0500

    Skeleton project

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model }  = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
 
@@ -14,9 +15,34 @@ const userSchema = new Schema({
     password:{
         type: String,
         required: true
+    },
+    role:{
+        type: String,
+        required: [true, 'Role is required'],
+        enum: ['ADMIN_AITECH_ROLE', 'USER_MODERADOR_ROLE']
+    },
+    status:{
+        type: Boolean,
+        default: true
     }
 
 });
+
+userSchema.method.toJSON = function(){
+    const { __v, password, _id, ...user } = this.toJSON();
+    user['uid'] = _id;
+    return user;
+}
+
+userSchema.methods.encryptPassword = async() => {
+    
+}
+
+userSchema.methods.comparePassword = async function( password ){
+    return await bcrypt.compare( password, this.password );
+}
+
+
 
 module.exports = model( 'User', userSchema )
 
